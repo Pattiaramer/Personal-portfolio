4e32fc8a306621ba01b04c95f80e09543fbb5263
Patti <74580622+Pattiaramer@users.noreply.github.com>
2022-06-22 16:21:08 -0700

    starting the homepage

---
+++ styles/nav-bar and extra code for later.vb
@@ -0,0 +1,42 @@
+   <!--Nav Bar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+      <a class="navbar-brand" href="#"
+        ><img src="patricia-araiza-merlin.png" alt=""
+      /></a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto justify-content-end">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Welcome! <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >About Me <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >My Work <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Contact <span class="sr-only">(current)</span></a
+            >
+          </li>
+          
+      </div>
+    </nav>
